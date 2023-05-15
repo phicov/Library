845efea42da53fbddbfd0666afc4f8000aabb442
Philippe Covington <philippehcbusi@gmail.com>
2023-05-15 14:10:55 -0400

    Featured Books

--- library-react/src/App.jsx
+++ library-react/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Nav from "./components/Nav";
 import Landing from "./components/Landing";
 import Highlights from "./components/Highlights";
+import Featured from "./components/Featured";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
