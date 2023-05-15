440619dcc8bc0529780818bad3a0ad09d6482061
Philippe Covington <philippehcbusi@gmail.com>
2023-05-15 12:52:06 -0400

    Highlight

--- library-react/src/App.jsx
+++ library-react/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Nav from "./components/Nav";
 import Landing from "./components/Landing";
+import Highlights from "./components/Highlights";
 
 function App() {
   return (
     <div>
       <Nav />
       <Landing />
+      <Highlights />
     </div>
   );
 }
