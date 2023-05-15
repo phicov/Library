aa316d4e3d45e9c793e35c14f0e4231bfc75d4b2
Philippe Covington <philippehcbusi@gmail.com>
2023-05-15 12:25:01 -0400

    Nav Bar

---
+++ library-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
