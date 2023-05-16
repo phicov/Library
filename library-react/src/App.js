df2cf907a2df1070eb6f83c43a2eb28254922ca9
Philippe Covington <philippehcbusi@gmail.com>
2023-05-16 12:58:56 -0400

    Cart page

--- library-react/src/App.js
+++ library-react/src/App.js
@@ -5,8 +5,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Books from "./pages/Books";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([])
   return (
     <Router>
       <div className="app">
@@ -14,7 +16,8 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/books" exact element={<Books books={books} />} />
-          <Route path="books/:id" element={<BookInfo books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} />} />
+          <Route path="/cart" element={<Cart books={books} />} />
         </Routes>
       </div>
     </Router>
